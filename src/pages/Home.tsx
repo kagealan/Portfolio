5c4bbb67752d19a5f31aea391ceda4a0506bec4a
Kagealan <kagealan33@gmail.com>
2024-02-23 04:57:30 +0100

    add:add img

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () => {
     return (
         <IonPage>
             <IonContent fullscreen>
-                <main className='h-full w-full font-[Poppins] overflow-hidden bg-gray-900 flex justify-center items-start p-5'>
+                <main className='h-full w-full font-[Poppins] overflow-hidden bg-gray-900 flex justify-center items-start p-5' style={{backgroundImage:'url(Image/bg5.png)',backgroundSize:'cover'}}>
                     <motion.div animate={{x:[-100,100],transition:{repeatType:'mirror',repeat:Infinity,type:'ease-in',duration:1}}} className="w-[5em] aspect-square rounded-full flex items-center justify-center">
                         <img src='favicon.png'></img>
                     </motion.div>
