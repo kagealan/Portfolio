b7066b432ca8d604022f468c3185b41565c46b7c
kagealan <kagealan@users.noreply.github.com>
2024-02-23 05:05:28 +0000

    deploy: 68f53db0fc2cc46a678b24d578cf77b5ecacdeb7

--- assets/keyboard2-legacy-0b7e62ce.js
+++ assets/keyboard2-legacy-0b7e62ce.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-bc8f27d8.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-8394db11.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- assets/status-tap-eedc9842.js
+++ assets/status-tap-eedc9842.js
@@ -1,3 +1,3 @@
-import{r as a,a as i,w as c}from"./index-de2ca686.js";import{f as d,s as l}from"./index8-45f004f2.js";/*!
+import{r as a,a as i,w as c}from"./index-56a7d6db.js";import{f as d,s as l}from"./index8-9a0750fb.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const p=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const n=e.innerWidth,s=e.innerHeight,o=document.elementFromPoint(n/2,s/2);if(!o)return;const t=d(o);t&&new Promise(r=>i(t,r)).then(()=>{c(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{p as startStatusTap};
