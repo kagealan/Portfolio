3be5cc19e74a926eed49dc16aca6b35d03bb1f31
Kagealan <kagealan33@gmail.com>
2024-02-23 06:17:20 +0100

    add:fix img

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home: React.FC = () => {
             <IonContent fullscreen>
                 <main className='h-full w-full font-[Poppins] overflow-hidden flex justify-center items-start p-5' style={{backgroundSize:'cover'}}>
                     <motion.div animate={{x:[-100,100],transition:{repeatType:'mirror',repeat:Infinity,type:'ease-in',duration:1}}} className="w-[5em] aspect-square rounded-full flex items-center justify-center">
-                        <img src='./favicon.png' alt='vite logo'></img>
+                        <img src='/favicon.png' alt='vite logo'></img>
                     </motion.div>
                 </main>
             </IonContent>
